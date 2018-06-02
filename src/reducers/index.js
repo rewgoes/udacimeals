9cfc0c47296715b98c63538d925f78f77d840224
Rafael Wolf de Goes <rewgoes@gmail.com>
2018-06-01 23:12:23 -0300

    feat: combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
+import { combineReducers } from 'redux';
 import {
   ADD_RECIPE,
   REMOVE_FROM_CALENDAR
@@ -88,4 +89,7 @@ function calendar(state = initialCalendarState, action) {
   }
 }
 
-export default calendar
+export default combineReducers({
+  food,
+  calendar
+})
