fbee6f9b8a6c785a516238139374d13ba536a7ea
Rafael Wolf de Goes <rewgoes@gmail.com>
2018-06-02 13:36:39 -0300

    feat: change logger to redux-logger

--- src/index.js
+++ src/index.js
@@ -4,17 +4,9 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware, compose } from 'redux'
+import logger from 'redux-logger'
 import reducer from './reducers'
 import { Provider } from 'react-redux'
-
-const logger = store => next => action => {
-  console.group(action.type)
-  console.info('dispatching', action)
-  let result = next(action)
-  console.log('next.state', store.getState())
-  console.groupEnd(action.type)
-  return result
-}
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
